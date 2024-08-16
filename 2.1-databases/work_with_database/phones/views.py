360b5c30524a409b29d2860e9cf6ec0193c142b8
Valeriy Predein <valeriypredein@gmail.com>
2024-08-16 15:21:11 +0300

    Выполнено задание 2.1

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +8,20 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    sort_pages = request.GET.get("sort")
+    phone_instanse = Phone.objects.all()
+    if sort_pages == "name":
+        phone_instanse = Phone.objects.order_by("name")
+    elif sort_pages == "max_price":
+        phone_instanse = Phone.objects.order_by("price").reverse()
+    elif sort_pages == "min_price":
+        phone_instanse = Phone.objects.order_by("price")
+    context = {"phones": phone_instanse}
+    return render(request, template, context=context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    phone = Phone.objects.get(slug=slug)
+    context = {'phone': phone}
+    return render(request, template, context=context)
